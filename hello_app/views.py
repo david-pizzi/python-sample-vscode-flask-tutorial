7d6b999758ba046cc5de0b34e53702adc493d17b
David Pizzi <david.pizzi@capgemini.com>
2024-01-02 00:12:39 +0000

    translator results

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,3 +1,7 @@
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
+
 from datetime import datetime
 from flask import Flask, render_template
 from . import app
@@ -6,9 +10,54 @@ from . import app
 def home():
     return render_template("home.html")
 
-@app.route("/translator/")
+@app.route("/translator/", methods=['GET'])
 def translator():
     return render_template("translator.html")
+
+@app.route('/translator/', methods=['POST'])
+def translator_post():
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    # Load the values from .env
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+    # Indicate that we want to translate and the API version (3.0) and the target language
+    path = '/translate?api-version=3.0'
+    # Add the target language parameter
+    target_language_parameter = '&to=' + target_language
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Set up the header information, which includes our subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
+
+    # Make the call using post
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    # Call render template, passing the translated text,
+    # original text, and target language to the template
+    return render_template(
+        'translator_results.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
 
 @app.route("/about/")
 def about():
